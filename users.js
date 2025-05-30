85542563aaa53849cc43fa6aab57c6153f7e5d92
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-05-30 16:44:54 +0530

    POST,PUT,DELETE

--- users.js
+++ users.js
@@ -25,13 +25,41 @@ router.get('/:id', (req, res) => {
 });
 
 // POST /user – Add new user
-router.post('/', validateUser, (req, res) => {
+router.post('/', (req, res) => {
   const newUser = {
     id: (users.length + 1).toString(),
     ...req.body
   };
   users.push(newUser);
   res.status(201).json(newUser);
+});
+
+
+// PUT /user/:id – Update existing user
+router.put('/:id', (req, res) => {
+  const user = users.find(user => user.id === req.params.id);
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  user.firstName = req.body.firstName;
+  user.lastName = req.body.lastName;
+  user.hobby = req.body.hobby;
+
+  res.status(200).json(user);
+});
+
+
+
+// DELETE /user/:id – Delete user
+router.delete('/:id', (req, res) => {
+  const index = users.findIndex(user => user.id === req.params.id);
+  if (index === -1) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  users.splice(index, 1);
+  res.status(200).json({ message: 'User deleted successfully' });
 });
 
 module.exports = router;
